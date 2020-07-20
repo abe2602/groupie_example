dba49c2edf7cb4234cef91bd7d9cfb59e004d149
Bruno Bacelar Abe <messengerabe@hotmail.com>
2020-07-20 10:51:51 -0300

    [master] Refactoring list itens

--- app/src/main/java/com/example/groupierecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/groupierecyclerview/MainActivity.kt
@@ -8,12 +8,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var numbersAdapter: NumbersAdapter
-    private val numbersList: List<Int> =
-            listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18)
+    private var numbersList: MutableList<Int> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for(i in 0..41){
+            numbersList.add(i)
+        }
 
         numbersAdapter = NumbersAdapter()
         numberRecyclerView.layoutManager = LinearLayoutManager(this)
